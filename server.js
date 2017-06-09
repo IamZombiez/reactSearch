0dc018d960e6ba9d08948935a93c8f84d5399cee
Derek Siu <derek@Dereks-MacBook-Pro.local>
2017-06-09 14:26:36 -0700

    starter files

---
+++ server.js
@@ -0,0 +1,45 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+var Note = require("./models/notes.js");
+var Article = require("./models/articles.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Initialize Express
+var app = express();
+
+// Morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static("public"));
+
+// DB Setup
+mongoose.connect("mongodb://localhost/nytreact");
+var db = mongoose.connection;
+
+// Err Check
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// Routes
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
