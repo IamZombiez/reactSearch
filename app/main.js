b84b644cc4c8eb84e101eae246723640d6656a9f
IamZombiez <dereksiu216@gmail.com>
2017-06-13 11:14:41 -0700

    file structure

--- app/main.js
+++ app/main.js
@@ -3,23 +3,14 @@ var React = require("react");
 
 // Including the Link component from React Router to navigate within our application without full page reloads
 var Link = require("react-router").Link;
+
 var Main = React.createClass({
   // Here we render the function
   render: function() {
     return (
       <div className="container">
-        <div className="jumbotron">
-          <h2><strong>H2 Header</strong></h2>
-          <p><em>React Routing Test</em></p>
-          <hr />
-          <p>
-            {/* <Link to="/Child1"><button className="btn btn-primary btn-lg">Show Child #1</button></Link>
-            <Link to="/Child2"><button className="btn btn-danger btn-lg">Show Child #2</button></Link> */}
-          </p>
-        </div>
         <div className="row">
-          {/* This code will dump the correct Child Component */}
-          {this.props.children}
+          <div className="col m12">Test Header</div>
         </div>
       </div>
     );
